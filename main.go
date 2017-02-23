f17528be078e9d6909558e67880b17603d34a0c9
Florin Stan <tom@tcnp3.com>
2017-02-22 22:03:08 -0700

    reorganization

--- main.go
+++ main.go
@@ -31,43 +31,46 @@ package main
 
 import (
 	"io/ioutil"
-	"log"
 	"os"
 
-	"github.com/tcncloud/protobuf/proto"
-	"github.com/tcncloud/protobuf/protoc-gen-go/plugin"
+	"github.com/Sirupsen/logrus"
+	"github.com/golang/protobuf/proto"
+	"github.com/golang/protobuf/protoc-gen-go/plugin"
+	"github.com/tcncloud/protoc-gen-persist/generator"
 )
 
+func init() {
+	if os.Getenv("DEBUG") != "" {
+		logrus.SetLevel(logrus.DebugLevel)
+	}
+}
+
 func Return(response *plugin_go.CodeGeneratorResponse) {
-	data, err := proto.Marshal(response)
-	if err != nil {
-		log.Fatal("That's wired ... ")
-	}
-	_, err = os.Stdout.Write(data)
-	if err != nil {
-		log.Fatal(os.Stderr, "I can't send data to stdout !")
-	}
 }
 
 func main() {
 	var req plugin_go.CodeGeneratorRequest
-	var res *plugin_go.CodeGeneratorResponse
 
 	data, err := ioutil.ReadAll(os.Stdin)
 	if err != nil {
-		res.Error = proto.String("Can't read the input")
-		Return(res)
-		return
+		logrus.Fatal("Can't read the stdin!")
 	}
 
 	if err := proto.Unmarshal(data, &req); err != nil {
-		res.Error = proto.String("Error parsing stdin data")
-		Return(res)
-		return
+		logrus.Fatal("Error parsing data!")
 	}
 	// DO processing
+	g := generator.NewGenerator(&req)
+	g.ProcessRequest()
 
 	// Send back the results.
-	Return(res)
+	data, err = proto.Marshal(g.GetResponse())
+	if err != nil {
+		logrus.Fatal("I can't serialize response")
+	}
+	_, err = os.Stdout.Write(data)
+	if err != nil {
+		logrus.Fatal("Can't send data to stdout!")
+	}
 
 }
