b7c7c854849ff41fc2e572dc3bfa90c571bc95ff
iamneal <neal.cooper@tcnbroadcasting.com>
2017-04-27 12:25:11 -0600

    added better examples of what hooks let you do

--- examples/mytime/hooks.go
+++ examples/mytime/hooks.go
@@ -7,19 +7,30 @@ import (
 	pb "github.com/tcncloud/protoc-gen-persist/examples/test"
 )
 
-var cache map[string]interface{}
+var cache map[int64]*pb.ExampleTable
 
 func init() {
-	cache = make(map[string]interface{})
+	cache = make(map[int64]*pb.ExampleTable)
 }
 
 func UniarySelectBeforeHook(req *pb.PartialTable) (*pb.ExampleTable, error) {
-	fmt.Printf("UniarySelectBeforeHook: %+v\n", req)
+	fmt.Printf("UniarySelectBeforeHook: cache: %#v\n", cache)
+	if req != nil {
+		res := cache[req.Id]
+		fmt.Printf("UniarySelectBeforeHook: req:%+v , cache: %+v\n", *req, res)
+		return res, nil
+	} else {
+		fmt.Println("UniarySelectBeforeHook: req was nil...")
+	}
 	return nil, nil
 }
 
-func UniarySelectAfterHook(res *pb.ExampleTable) error {
-	fmt.Printf("uniarySelectAfterHook: %+v\n", res)
+func UniarySelectAfterHook(req *pb.PartialTable, res *pb.ExampleTable) error {
+	if req != nil {
+		fmt.Printf("uniarySelectAfterHook: req:%+v , res:%+v\n", *req, *res)
+		cache[req.Id] = res
+	}
+	fmt.Printf("UniarySelectAfterHook: cache now: %#v\n", cache)
 	return nil
 }
 
@@ -28,7 +39,7 @@ func ServerStreamBeforeHook(req *pb.Name) (*pb.ExampleTable, error) {
 	return nil, nil
 }
 
-func ServerStreamAfterHook(res *pb.ExampleTable) error {
+func ServerStreamAfterHook(req *pb.Name, res *pb.ExampleTable) error {
 	fmt.Printf("ServerStreamAfterHook: %+v\n", res)
 	return nil
 }
@@ -38,7 +49,7 @@ func BidirectionalBeforeHook(req *pb.ExampleTable) (*pb.ExampleTable, error) {
 	return nil, nil
 }
 
-func BidirectionalAfterHook(res *pb.ExampleTable) error {
+func BidirectionalAfterHook(req *pb.ExampleTable, res *pb.ExampleTable) error {
 	fmt.Printf("BidirectionalAfterHook: %+v\n", res)
 	return nil
 }
@@ -46,4 +57,14 @@ func BidirectionalAfterHook(res *pb.ExampleTable) error {
 func ClientStreamBeforeHook(req *pb.ExampleTable) (*pb.NumRows, error) {
 	fmt.Printf("ClientStreamBeforeHook: %+v\n", req)
 	return nil, nil
+}
+
+func ClientStreamAfterHook(req *pb.ExampleTable, res *pb.Ids) error {
+	if res != nil {
+		fmt.Printf("ClientStreamAfterHook adding to Ids. so far collected: %+v\n", *res)
+		res.Ids = append(res.Ids, req.Id)
+	} else {
+		fmt.Println("ClientStreamingAfterHook res was nil...")
+	}
+	return nil
 }
